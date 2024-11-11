6cdd415bbdb46df9ea46d2ca1a9b8956f227c3f6
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-11 19:50:19 +0530

    create packages

--- src/main/java/lk/ijse/gdse67/greenShadow/controller/HealthCheckController.java
+++ src/main/java/lk/ijse/gdse67/greenShadow/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package lk.ijse.gdse67.greenShadow;
+package lk.ijse.gdse67.greenShadow.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
