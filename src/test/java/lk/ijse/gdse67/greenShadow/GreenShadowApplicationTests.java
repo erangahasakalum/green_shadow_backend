7aabaafccd61fd3b94da9123d76b38abde8601c7
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-11 11:48:58 +0530

    Initial commit

---
+++ src/test/java/lk/ijse/gdse67/greenShadow/GreenShadowApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse67.greenShadow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShadowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
