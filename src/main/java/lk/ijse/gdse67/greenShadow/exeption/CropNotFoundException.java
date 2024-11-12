a41fb00305d5a999328eb79addd4dbded1b38c0c
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-12 08:52:55 +0530

    some changes in CropNotFoundException class

--- src/main/java/lk/ijse/gdse67/greenShadow/exeption/CropNotFoundException.java
+++ src/main/java/lk/ijse/gdse67/greenShadow/exeption/CropNotFoundException.java
@@ -2,7 +2,7 @@ package lk.ijse.gdse67.greenShadow.exeption;
 
 public class CropNotFoundException extends RuntimeException{
     public CropNotFoundException() {
-        super();
+
     }
 
     public CropNotFoundException(String message, Throwable cause) {
