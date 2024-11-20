b799066d267b32c0e8e15745a59655e726d600ae
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-20 14:33:19 +0530

    add base64 convert pic method

--- src/main/java/lk/ijse/gdse67/greenShadow/utill/AppUtil.java
+++ src/main/java/lk/ijse/gdse67/greenShadow/utill/AppUtil.java
@@ -1,8 +1,13 @@
 package lk.ijse.gdse67.greenShadow.utill;
 
+import java.util.Base64;
 import java.util.UUID;
 
 public class AppUtil {
+    public static String cropImage(byte[] image){
+        return Base64.getEncoder().encodeToString(image);
+    }
+
     public static String generateUserId() {
         return "USER-"+UUID.randomUUID();
     }
