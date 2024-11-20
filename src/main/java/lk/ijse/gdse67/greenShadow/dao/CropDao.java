ad65c3b6d04e1c6e5fbe52826e4b53318ce91fa0
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-20 14:43:03 +0530

    add id generate method crop dao

--- src/main/java/lk/ijse/gdse67/greenShadow/dao/CropDao.java
+++ src/main/java/lk/ijse/gdse67/greenShadow/dao/CropDao.java
@@ -1,7 +1,12 @@
 package lk.ijse.gdse67.greenShadow.dao;
 
 import lk.ijse.gdse67.greenShadow.entity.impl.CropEntity;
+import lk.ijse.gdse67.greenShadow.entity.impl.VehicleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CropDao extends JpaRepository<CropEntity,String> {
+    @Query(value = "SELECT * FROM crop WHERE crop_id = (SELECT crop_id FROM crop ORDER BY CAST(SUBSTRING(crop_id, 5) AS UNSIGNED) DESC LIMIT 1);"
+            , nativeQuery = true)
+    VehicleEntity findLastRowNative();
 }
