392399ad6f110763f7b4337142f21801186e320a
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-25 22:46:35 +0530

    staff save done

--- src/main/java/lk/ijse/gdse67/greenShadow/controller/StaffController.java
+++ src/main/java/lk/ijse/gdse67/greenShadow/controller/StaffController.java
@@ -1,6 +1,11 @@
 package lk.ijse.gdse67.greenShadow.controller;
 import lk.ijse.gdse67.greenShadow.dto.impl.StaffDTO;
+import lk.ijse.gdse67.greenShadow.exeption.DataPersistException;
+import lk.ijse.gdse67.greenShadow.service.StaffService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/staff")
 public class StaffController {
 
-    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE ,consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void saveStaff(@RequestBody StaffDTO staffDTO) {
+    @Autowired
+    private StaffService staffService;
 
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO) {
+        try {
+            staffService.saveStaff(staffDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (DataPersistException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- src/main/java/lk/ijse/gdse67/greenShadow/service/impl/StaffServiceImpl.java
+++ src/main/java/lk/ijse/gdse67/greenShadow/service/impl/StaffServiceImpl.java
@@ -1,12 +1,43 @@
 package lk.ijse.gdse67.greenShadow.service.impl;
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse67.greenShadow.dao.StaffDao;
 import lk.ijse.gdse67.greenShadow.dto.impl.StaffDTO;
+import lk.ijse.gdse67.greenShadow.entity.impl.EquipmentEntity;
+import lk.ijse.gdse67.greenShadow.entity.impl.StaffEntity;
+import lk.ijse.gdse67.greenShadow.exeption.DataPersistException;
 import lk.ijse.gdse67.greenShadow.service.StaffService;
+import lk.ijse.gdse67.greenShadow.utill.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Transactional
+@Service
 public class StaffServiceImpl implements StaffService {
-    @Override
-    public void saveStaff(StaffDTO staff) {
 
+    @Autowired
+    private StaffDao staffDao;
+
+    @Autowired
+    private Mapping staffMapping;
+    @Override
+    public void saveStaff(StaffDTO staffDTO) {
+        int id = 0;
+        StaffEntity lastId = staffDao.findLastRowNative();
+        if (lastId != null) {
+            try {
+                String[] split = lastId.getMemberCode().split("-");
+                id = Integer.parseInt(split[0]);
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException("Invalid member code");
+            }
+        }
+        staffDTO.setMemberCode("MEMBER -" + ++id);
+        StaffEntity save = staffDao.save(staffMapping.toStaffEntity(staffDTO));
+        if (save == null) {
+            throw new DataPersistException("vehicle not saved");
+        }
     }
 
     @Override
