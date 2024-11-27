b5e06c5ffd974841146614a0508465dc10edfd08
Eranga Hasakalum <erangahasakalum6761@gmail.com>
2024-11-27 11:26:16 +0530

    vehicle save bug fix

--- src/main/java/lk/ijse/gdse67/greenShadow/dao/VehicleDao.java
+++ src/main/java/lk/ijse/gdse67/greenShadow/dao/VehicleDao.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface VehicleDao extends JpaRepository<VehicleEntity,String> {
-    @Query(value = "SELECT * FROM vehicle WHERE vehicle_code = (SELECT vehicle_code FROM crop ORDER BY CAST(SUBSTRING(vehicle_code, 9) AS UNSIGNED) DESC LIMIT 1);"
+    @Query(value = "SELECT * FROM vehicle WHERE vehicle_code = (SELECT vehicle_code FROM vehicle ORDER BY CAST(SUBSTRING(vehicle_code, 9) AS UNSIGNED) DESC LIMIT 1);"
             , nativeQuery = true)
     VehicleEntity findLastRowNative();
 }
